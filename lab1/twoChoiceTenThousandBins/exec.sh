fc22d3c758b69265470189c6b0fe470df0777868
AleexHrB <alexherrerobravo@gmail.com>
2025-11-09 18:24:31 +0100

    two choice

---
+++ lab1/twoChoiceTenThousandBins/exec.sh
@@ -0,0 +1,19 @@
+
+folder="oneChoiceHundredBins"
+m=100
+rm *.txt
+cd ..
+make 
+
+for i in $(seq 1 1000)
+do
+    echo "Running $i"
+    n=10
+    while [ $n -le $((m*m)) ]
+    do
+        ./bins $n $m 0.00 >> $folder/oneChoice$n.txt
+        n=$((10*n))
+    done
+done
+
+make clean
