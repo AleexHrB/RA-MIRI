66591106a0eca6f91479c5083410b54845e78340
lluna-cc <llunaclavera@gmail.com>
2025-11-12 12:04:12 +0100

    csv y experimetnos

--- lab1/batchedExperiments/betaChoiceThousandBins/exec.sh
+++ lab1/batchedExperiments/betaChoiceThousandBins/exec.sh
@@ -1,23 +1,24 @@
 
 folder=$(pwd)
-m=100
+m=1000
 rm *.txt
 cd ..
 make 
 
-
 for perc in 0.01 0.05 0.10 0.50 1.00
 do
-    for i in $(seq 1 1000)
+    for beta in 0.25 0.5 0.75
     do
-        echo "Running $i"
-        n=10
-        while [ $n -le $((m*m)) ]
+        for i in $(seq 1 200)
         do
-            ./bins $n $m 1.00 $perc >> $folder/twoChoice$n.txt
-            n=$((10*n))
+            echo "Running $i"
+            n=100
+            while [ $n -le $((m*m)) ]
+            do
+                ./bins $n $m $beta $perc >> $folder/betaChoice$n-$beta-$perc.txt
+                n=$((10*n))
+            done
         done
     done
 done
-
 make clean
