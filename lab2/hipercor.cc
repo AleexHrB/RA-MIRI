76a8fc437d3938bd4375325e74bdd7ea4d7c9fd6
lluna-cc <llunaclavera@gmail.com>
2025-12-27 18:22:45 +0100

    hyperloglog almost done

--- lab2/hipercor.cc
+++ lab2/hipercor.cc
@@ -2,6 +2,7 @@
 #include <set>
 #include <cmath>
 #include "xxhash32.h"
+#include <vector>
 using namespace std;
 
 unsigned int firstBit(unsigned int x) {
@@ -19,19 +20,31 @@ unsigned int firstBit(unsigned int x) {
 int main(int argc, char**argv) {
     unsigned int k = atoi(argv[1]);
     XXHash32 h(0);
-    unsigned int p = h.hash(&k, sizeof(k), 0);
-    cout << p << endl;
+    //unsigned int p = h.hash(&k, sizeof(k), 0);
 
     string elem;
-    unsigned int r = 0;
-    
+    unsigned int b = 4;
+    unsigned int rest = 32 - b;
+    unsigned int m = 1 << b;
+    vector<unsigned int> R(m);
+
+
     while(cin >> elem) {
         const char* prueba = elem.c_str();
         unsigned int hashVal = h.hash(prueba, elem.length(), 0);
-        unsigned int firstOne = firstBit(hashVal);
-        r = firstOne > r ? firstOne : r;
+        unsigned int j = hashVal >> rest;
+        unsigned int w = hashVal << rest;
+            
+        unsigned int firstOne = firstBit(w);
+        R[j] = firstOne > R[j] ? firstOne : R[j];
     }
+    double Z = 0.0;
+    for (unsigned int j = 0; j < m; ++j) {
+       Z += 1.0/double(1 << R[j]); 
+    }
+    Z = 1.0/Z;
 
-    cout << r << endl;
+    double E = m*m*Z;
+    cout << E << endl;
 }
 
