e779e0a640097aaf1932e7f0c3702af3edfa045a
lluna-cc <llunaclavera@gmail.com>
2025-12-27 19:37:38 +0100

    hyperloglog

--- lab2/hipercor.cc
+++ lab2/hipercor.cc
@@ -8,8 +8,7 @@ using namespace std;
 unsigned int firstBit(unsigned int x) {
     unsigned int i = 1;
     if (x == 0) return 33;
-    while (!(x & 0x80000000)) {
-        x <<= 1;
+    while (!(x & (1 << (32 - i)))) {
         ++i;
     }
 
@@ -19,32 +18,38 @@ unsigned int firstBit(unsigned int x) {
 
 int main(int argc, char**argv) {
     unsigned int k = atoi(argv[1]);
-    XXHash32 h(0);
-    //unsigned int p = h.hash(&k, sizeof(k), 0);
+    XXHash32 h(k);
+    unsigned int p = h.hash(&k, sizeof(k), 0);
 
     string elem;
     unsigned int b = 4;
-    unsigned int rest = 32 - b;
     unsigned int m = 1 << b;
-    vector<unsigned int> R(m);
+    vector<unsigned int> R(m,0);
+    double alphas[8] = {0.0438992,0.133109,0.312804355469,0.673102023868, 1.39424526760, 2.8368338114801, 5.7221695197, 11.4929222111};
 
 
     while(cin >> elem) {
         const char* prueba = elem.c_str();
-        unsigned int hashVal = h.hash(prueba, elem.length(), 0);
-        unsigned int j = hashVal >> rest;
-        unsigned int w = hashVal << rest;
+        unsigned int hashVal = h.hash(prueba, elem.length(), k);
+        unsigned int j = hashVal >> (sizeof(int)*8 - b);
+        unsigned int w = hashVal << b;
             
         unsigned int firstOne = firstBit(w);
         R[j] = firstOne > R[j] ? firstOne : R[j];
+    
     }
     double Z = 0.0;
     for (unsigned int j = 0; j < m; ++j) {
-       Z += 1.0/double(1 << R[j]); 
+       Z += 1.0/pow(2,R[j]);
     }
     Z = 1.0/Z;
 
-    double E = m*m*Z;
+    //double alpha = tgamma(-1.0/m) * ((1 - pow(2.0,1.0/m))/log(2));
+    //alpha = 1/pow(alpha,m);
+
+    double alpha = alphas[b - 1];
+    double E = m*m*Z * alpha;
+    
     cout << E << endl;
 }
 
