7aeb7be746c7711815937cf1f701bd9b40dedc2f
AleexHrB <alexherrerobravo@gmail.com>
2025-12-28 15:43:33 +0100

    alphas

--- lab2/checkAlpha.sh
+++ lab2/checkAlpha.sh
@@ -1,6 +1,9 @@
 make clean
 make
 rm *.txt
+cd synthethicAlpha
+rm *Solution*
+cd ..
 for f in $(ls synthethicAlpha/*.txt)
 do
     echo $f
@@ -8,8 +11,21 @@ do
     for i in $(seq 1 500)
     do
         echo $i
-        newName="${newf}Solution.txt"
+        newName="${newf}SolutionHyper.txt"
         ./hipercor 4 < $f >> $newName
+    done
+    echo "==========================="
+done
+
+for f in $(ls synthethicAlpha/*.txt)
+do
+    echo $f
+    newf=$(basename -- "$f" .txt)
+    for i in $(seq 1 500)
+    do
+        echo $i
+        newName="${newf}SolutionRecord.txt"
+        ./main 16 < $f >> $newName
     done
     echo "==========================="
 done
